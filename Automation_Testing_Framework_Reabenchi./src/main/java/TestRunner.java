f14f5a4e168e93b818df7320ce867029b2f7ed87
iuriereabenchi <reabenchi91@ymail.com>
2023-03-27 15:39:45 +0300

    homework 17

--- Automation_Testing_Framework_Reabenchi./src/main/java/TestRunner.java
+++ Automation_Testing_Framework_Reabenchi./src/main/java/TestRunner.java
@@ -1,9 +1,14 @@
 import Managers.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.RegisterPage;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -12,17 +17,22 @@ public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager chromeDriverManager = new WebDriverManager("CHROME");
-        chromeDriverManager.getDriver().get("https://www.autodoc.de/");
-        Thread.sleep(10000);
-        chromeDriverManager.closeDriver();
+        chromeDriverManager.getDriver().get("https://demo.opencart.com/");
+        HomePage homePage = new HomePage(chromeDriverManager.getDriver());
+        homePage.navigateToRegisterPage();
+        RegisterPage registerPage = new RegisterPage(chromeDriverManager.getDriver());
+        registerPage.fillInTheRegisterForm("vlad", "ignatenco", "ignatencovlad@mail.ru", "11111");
+        Thread.sleep(3000);
 
-        //WebDriverManager firefoxDriver = new WebDriverManager("FIREFOX");
-        //firefoxDriver.getDriver().get("https://stackoverflow.com/questions/75709476/selenium-java-not-secure-data-displays-on-chrome-browser");
-        //Thread.sleep(10000);
-        //firefoxDriver.closeDriver();
+
+        LoginPage loginPage = new LoginPage(chromeDriverManager.getDriver());
+        loginPage.navigateToLoginPage();
+        loginPage.fillLoginPaga("ignatenco@gmail.com", "11111a");
+        Thread.sleep(3000);
+        loginPage.pressLogin();
+
+
     }
 }
-
-
 
 
