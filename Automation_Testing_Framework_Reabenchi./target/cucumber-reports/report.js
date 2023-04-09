dfdd372a5dbf93b09785860ff8dd7dca1a02e86b
iuriereabenchi <reabenchi91@ymail.com>
2023-04-09 18:04:49 +0300

    homework 19

---
+++ Automation_Testing_Framework_Reabenchi./target/cucumber-reports/report.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Register.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Register feature test suite",
+  "description": "",
+  "id": "register-feature-test-suite",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Register page is accessible from Home Page",
+  "description": "",
+  "id": "register-feature-test-suite;register-page-is-accessible-from-home-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "\"https://demo.opencart.com/\" is accessed",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "my account button is clicked",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "register account button is clicked",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "the new url contains the following string \"register\"",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "my account button is displayed on Home Page",
+  "description": "",
+  "id": "register-feature-test-suite;my-account-button-is-displayed-on-home-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 12,
+  "name": "\"https://demo.opencart.com/\" is accessed",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 13,
+  "name": "my Account button is clicked",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "register account botton is displayed",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
